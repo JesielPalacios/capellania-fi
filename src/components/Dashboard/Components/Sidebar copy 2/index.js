c14e540f7b00bcc6dcd282161927baeed112a44a
Jesiel Palacios <jopalacioss@unac.edu.co>
2022-05-12 07:46:45 -0500

    Add latest changes

--- src/components/Dashboard/Components/Sidebar copy 2/index.js
+++ src/components/Dashboard/Components/Sidebar copy 2/index.js
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import { routes } from '../../../../core/router/routes'
 import './styles.css'
@@ -6,35 +6,55 @@ import './styles.css'
 export const Sidebar = ({ showSidebar, sidebar }) => {
   let itemList = useRef()
   let list = useRef()
+  let currentItemList
 
   const currentItem = (index) => {
-    // for (let index = 0; index < list.current.children.length; index++) {
-    //   list.current.children[index].className = 'list'
-    // }
-    // if (list.current.children[index].className === 'list') {
-    //   list.current.children[index].className = 'list active'
-    // } else {
-    //   list.current.children[index].className = 'list'
-    // }
+    console.clear()
+    console.log(index)
+    // console.log(list)
+    // console.log(list.current)
+    // console.log(list.current.children)
+    // console.log(list.current.childNodes)
+    // console.log(list.current.children[index])
+
+    // Position of the anchor element
+    // console.log(list.current.children[index].children[2])
+    // console.log(list.current.children[index].children[2].offsetParent)
+
     for (let index = 0; index < list.current.children.length; index++) {
-      // console.log(list.current.children[index].children)
-      for (
-        let indexj = 0;
-        indexj < list.current.children[index].children.length;
-        indexj++
-      ) {
+      list.current.children[index].className = 'list'
+    }
+
+    list.current.children[index].children[2].offsetParent.className =
+      'list active'
+  }
+
+  useEffect(() => {
+    console.clear()
+
+    for (let i = 0; i < list.current.children.length; i++) {
+      console.log('Anchor number: ', i+1);
+      // console.log(list.current.children[i])
+      // console.log(list.current.children[i].children)
+      for (let j = 0; j < list.current.children[i].children.length; j++) {
+        // console.log(list.current.children[i].children[j])
+        // console.log(list.current.children[i].children[j].className)
+        
+        console.log(list.current.children[i].children[2])
+        // currentItemList = list.current.children[i].children[2]
         // if (
-        //   list.current.children[index].children[indexj].className = 'a.activee'
-        // ) {
+          //   list.current.children[index].children[indexj].className = 'a.activee'
+          // ) {
         //   console.log(list.current.children[index].children[indexj])
         // }
-        // console.log(list.current.children[index].children[indexj].className)
       }
     }
-    
-    // Imprime la clase de 
+
+    // console.log(currentItemList)
+
+    // Imprime la clase de
     // console.log(list.current.children[index].className)
-  }
+  }, [])
 
   return (
     <>
