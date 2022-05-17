ce966ba33323772e18a424715320b46f06e96fd0
Anthony Romero <iaromerod@unac.edu.co>
2022-05-16 23:34:32 -0500

    Update LoginÂ

---
+++ src/components/Login/ElementIndex2.js
@@ -0,0 +1,128 @@
+import styled from "styled-components";
+
+export const Logo = styled.img`
+  position: relative;
+  width: 226px;
+  height: 248px;
+  left: 115px;
+  margin-top: 0 auto;
+  cursor: pointer;
+`;
+
+export const Welcome = styled.h2`
+  position: absolute;
+  width: 70px;
+  height: 28px;
+  left: 205px;
+  margin-top: 0 auto;
+  text-align: center;
+  align-items: center;
+`;
+
+export const LoginCont = styled.div`
+position: relative;
+  display: flex;
+`;
+
+export const LeftSide = styled.div`
+  flex-direction: column;
+  align-items:center;
+`;
+
+export const EmailLabel = styled.label`
+  position: absolute;
+  width: 104px;
+  height: 22px;
+  left: 50px;
+  top: 330px;
+
+  font-family: "Nunito";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 27px;
+  /* identical to box height */
+
+  letter-spacing: -0.103101px;
+
+  color: #030229;
+`;
+
+export const EmailInput = styled.input`
+  position: absolute;
+  width: 348px;
+  height: 50px;
+  left: 50px;
+  top: 360px;
+
+  background: #f7f7f8;
+  border: 2px solid #ffffff;
+  box-sizing: border-box;
+  border-radius: 10px;
+`;
+
+export const ContrasenaLabel = styled.label`
+  position: absolute;
+
+  width: 71px;
+  height: 22px;
+  left: 50px;
+  top: 399px;
+
+  font-family: "Nunito";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 27px;
+  /* identical to box height */
+
+  letter-spacing: -0.103101px;
+
+  color: #030229;
+  margin-top: 25px;
+`;
+
+export const ContrasenaInput = styled.input`
+  position: absolute;
+  width: 348px;
+  height: 50px;
+  left: 50px;
+  top: 454px;
+
+  background: #f7f7f8;
+  border: 2px solid #ffffff;
+  box-sizing: border-box;
+  border-radius: 10px;
+`;
+
+export const Remember = styled.input`
+  position: absolute;
+  width: 154px;
+  height: 19px;
+  left: 50px;
+  top: 500px;
+`;
+
+export const ResetPassw = styled.a`
+  position: absolute;
+  width: 139px;
+  height: 19px;
+  left: 259px;
+  top: 510px;
+  color: #605bff;
+`;
+
+export const Enviar = styled.button`
+  position: absolute;
+  width: 100px;
+  height: 40px;
+  left: 166px;
+  top: 560px;
+  border-radius: 10px;
+  border: 2px;
+  background-color: #2A5784;
+`;
+
+export const RightSide = styled.div`
+  background: #e5e9ec;
+`;
--- src/components/Login/index 2.js
+++ src/components/Login/index 2.js
@@ -1,142 +1,154 @@
 import React from "react";
-import styled from "styled-components";
-import LogoImg from "../../assets/img/logo-unac.png";
-import InstagramImg from "../../assets/img/instagram.png"; // Reemplazar por el logo de Gmail
-import FacebookImg from "../../assets/img/facebook.png";
+
+import LogoImg from "../../assets/img/logo-unac-yellow.png";
+import {
+  ContrasenaInput,
+  ContrasenaLabel,
+  EmailInput,
+  EmailLabel,
+  Enviar,
+  LeftSide,
+  LoginCont,
+  Logo,
+  Remember,
+  ResetPassw,
+  RightSide,
+  Welcome,
+} from "./ElementIndex2";
 
 export const Login = () => {
   return (
-    <MainLogin>
-      Login
-      <LoginCont>
-        <LeftSide>
-          <Logo src={LogoImg} />
-          Log in
-          <form>
-            <EmailLabel for="email">Email Address</EmailLabel>
-            <EmailInput
-              placeholder="example@gmail.com"
-              type="email"
-              id="email"
-            />
-            <ContrasenaLabel for="passw">Password</ContrasenaLabel>
-            <ContrasenaInput
-              placeholder="• • • • • • •"
-              type="password"
-              id="passw"
-            />
-            <Enviar>Entrar</Enviar>
-            <Remember>
-              {/* <input type='checkbox'>Recordar contraseña</input> */}
-              <h4>Recordar Password</h4>
-            </Remember>
-          </form>
-        </LeftSide>
-      </LoginCont>
-    </MainLogin>
+    <LoginCont>
+      <LeftSide>
+        <Logo src={LogoImg} />
+        <Welcome>Log in</Welcome>
+        <form>
+          <EmailLabel for="email">Email Address</EmailLabel>
+          <EmailInput placeholder=" example@gmail.com" type="email" id="email" />
+          <ContrasenaLabel for="passw">Password</ContrasenaLabel>
+          <ContrasenaInput
+            placeholder=" • • • • • • •"
+            type="password"
+            id="passw"
+          />
+          <Enviar>Entrar</Enviar>
+          {/* <Remember type="checkbox">Recordar contraseña</Remember> */}
+          <ResetPassw>¿Reset Password?</ResetPassw>
+        </form>
+      </LeftSide>
+      <RightSide></RightSide>
+    </LoginCont>
   );
 };
 
-const MainLogin = styled.div``;
+// const Logo = styled.img`
+//   position: relative;
+//   width: 226px;
+//   height: 248px;
+//   left: 115px;
+//   top: 84px;
+//   cursor: pointer;
+// `;
 
-const Logo = styled.img`
-  position: relative;
-  width: 226px;
-  height: 248px;
-  left: 115px;
-  top: 84px;
-  cursor: pointer;
-`;
+// const Welcome = styled.h2`
+//   width: 70px;
+//   height: 28px;
+//   left: 189px;
+//   top: 332px;
+// `;
 
-const LoginCont = styled.div``;
+// const LoginCont = styled.div``;
 
-const LeftSide = styled.div``;
+// const LeftSide = styled.div`
+//   border-radius: 2px;
+//   box-shadow: 100%;
+// `;
 
-const EmailLabel = styled.label`
-  position: absolute;
-  width: 104px;
-  height: 22px;
-  left: 50px;
-  top: 527px;
+// const EmailLabel = styled.label`
+//   position: absolute;
+//   width: 104px;
+//   height: 22px;
+//   left: 50px;
+//   top: 527px;
 
-  font-family: "Nunito";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 27px;
-  /* identical to box height */
+//   font-family: "Nunito";
+//   font-style: normal;
+//   font-weight: 400;
+//   font-size: 16px;
+//   line-height: 27px;
+//   /* identical to box height */
 
-  letter-spacing: -0.103101px;
+//   letter-spacing: -0.103101px;
 
-  color: #030229;
-`;
+//   color: #030229;
+// `;
 
-const EmailInput = styled.input`
-  position: absolute;
-  width: 348px;
-  height: 50px;
-  left: 50px;
-  top: 557px;
+// const EmailInput = styled.input`
+//   position: absolute;
+//   width: 348px;
+//   height: 50px;
+//   left: 50px;
+//   top: 557px;
 
-  background: rgba(255, 255, 255, 0.47);
-  border: 2px solid #ad3c40;
-  box-sizing: border-box;
-  border-radius: 10px;
+//   background: rgba(255, 255, 255, 0.47);
+//   border: 2px solid #ad3c40;
+//   box-sizing: border-box;
+//   border-radius: 10px;
 
-  margin-top: 5px;
-`;
+//   margin-top: 5px;
+// `;
 
-const ContrasenaLabel = styled.label`
-  position: absolute;
-  /* Contraseña */
+// const ContrasenaLabel = styled.label`
+//   position: absolute;
+//   /* Contraseña */
 
-  /* position: absolute; */
-  width: 71px;
-  height: 22px;
-  left: 50px;
-  top: 627px;
+//   /* position: absolute; */
+//   width: 71px;
+//   height: 22px;
+//   left: 50px;
+//   top: 627px;
 
-  font-family: "Nunito";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 21px;
-  /* identical to box height */
+//   font-family: "Nunito";
+//   font-style: normal;
+//   font-weight: 400;
+//   font-size: 14px;
+//   line-height: 21px;
+//   /* identical to box height */
 
-  letter-spacing: -0.103101px;
+//   letter-spacing: -0.103101px;
 
-  color: #030229;
-  margin-top: 25px;
-`;
+//   color: #030229;
+//   margin-top: 25px;
+// `;
 
-const ContrasenaInput = styled.input`
-  position: absolute;
-  width: 348px;
-  height: 50px;
-  left: 50px;
-  top: 657px;
+// const ContrasenaInput = styled.input`
+//   position: absolute;
+//   width: 348px;
+//   height: 50px;
+//   left: 50px;
+//   top: 657px;
 
-  background: rgba(255, 255, 255, 0.47);
-  border: 2px solid #ffffff;
-  box-sizing: border-box;
-  border-radius: 10px;
+//   background: rgba(255, 255, 255, 0.47);
+//   border: 2px solid #ffffff;
+//   box-sizing: border-box;
+//   border-radius: 10px;
 
-  margin-top: 5px;
-`;
+//   margin-top: 5px;
+// `;
 
-const Enviar = styled.button`
-  position: absolute;
-  width: 123px;
-  height: 51px;
-  left: 169px;
-  top: 782px;
-`;
-const Remember = styled.div`
-  position: absolute;
-  width: 154px;
-  height: 19px;
-  left: 50px;
-  top: 732px;
-`;
+// const Enviar = styled.button`
+//   position: absolute;
+//   width: 123px;
+//   height: 51px;
+//   left: 169px;
+//   top: 782px;
+// `;
+// const Remember = styled.div`
+//   position: absolute;
+//   width: 154px;
+//   height: 19px;
+//   left: 50px;
+//   top: 732px;
+// `;
 
-const RightSide = styled.div``;
+// const RightSide = styled.div``;
