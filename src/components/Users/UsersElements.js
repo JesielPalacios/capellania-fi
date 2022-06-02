998f37b1c07eba79b959623eae2e7e24cd3623cb
Anthony Romero <iaromerod@unac.edu.co>
2022-06-02 16:43:52 -0500

    Adding user list

---
+++ src/components/Users/UsersElements.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const UserCont = styled.div`
+background-color: #fff;
+`
+export const UserTarjet = styled.div` 
+display: flex;
+/* flex-wrap: nowrap; */
+box-shadow: 0 8px rgba(0,0,0,0.2);
+`
--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -1,12 +1,21 @@
-import useUser from '../../core/hooks/useUser'
-import { Main } from '../Dashboard/Components/Main'
+import useUser from "../../core/hooks/useUser";
+import { Main } from "../Dashboard/Components/Main";
+import { UserCont, UserTarjet } from "./UsersElements";
 
 export const Users = () => {
-  const { sidebar } = useUser()
+  const { sidebar } = useUser();
 
   return (
-    <Main title={'Usuarios'} sidebar={sidebar}>
-      Usuarios
+    <Main title={"Usuarios"} sidebar={sidebar}>
+      <UserCont>
+        <UserTarjet> <button> users </button> </UserTarjet>
+      </UserCont>
+      <UserCont>
+        <UserTarjet> <button> users </button> </UserTarjet>
+      </UserCont>
+      <UserCont>
+        <UserTarjet> <button> users </button> </UserTarjet>
+      </UserCont>
     </Main>
-  )
-}
+  );
+};
