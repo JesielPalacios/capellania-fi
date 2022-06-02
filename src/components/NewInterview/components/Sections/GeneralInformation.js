3cdda58a6698ddffded88509b0d0d41a187f5e9a
Jesiel Palacios <jopalacioss@unac.edu.co>
2022-06-02 18:00:30 -0500

    Add latest changes

--- src/components/NewInterview/components/Sections/GeneralInformation.js
+++ src/components/NewInterview/components/Sections/GeneralInformation.js
@@ -2,20 +2,6 @@ import React from 'react'
 
 export const GeneralInformation = () => {
   return (
-    {fields.map(
-                (
-                  { field, placeholder, select, options, large, type },
-                  index,
-                  fieldItem
-                ) => {
-                  return (
-                    <FieldWrapper key={index} large={large}>
-                      {(flag = false)}
-                      <LabelForm htmlFor={field}>{field}</LabelForm>
-                      {ShowInput(select, field, options, placeholder, type)}
-                    </FieldWrapper>
-                  )
-                }
-              )}
+    <div>GeneralInformation</div>
   )
 }
