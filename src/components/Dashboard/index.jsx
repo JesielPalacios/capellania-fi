3764746789ba0994fe773eb110c4ea100852685a
Jesiel Palacios <jopalacioss@unac.edu.co>
2022-08-02 17:14:04 -0500

    Adds latest changes

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -10,15 +10,46 @@ export const Dashboard2 = () => {
   const { sidebar } = useUser()
   let { path, url } = useRouteMatch()
 
+  const show = () => {
+    // switch (path) {
+    //   case '/dashboard':
+    //     return Dashboard
+    //   case '/users':
+    //     return Users
+    //   case '/entrevistas':
+    //     return Interviews
+    //   default:
+    //     return Dashboard
+    // }
+    switch (path) {
+      case '/dashboard':
+        return <Route path={`${path}`} component={Dashboard} />
+      case '/usuarios':
+        return <Route path={`${path}`} component={Users} />
+      case '/entrevistas':
+        return <Route path={`${path}`} component={Interviews} /> 
+      default:
+        return <Route path={`${path}`} component={Dashboard} />
+    }
+  }
+
   return (
     <DashboardContainer sidebar={sidebar}>
-      <Route exact path={path}>
+      {/* <Route exact path={path}>
         <Sidebar />
-      </Route>
+      </Route> */}
+      <Sidebar />
+
+      {/* {() => console.log('Hola')} */}
       <Switch>
-        <Route path={`${path}/dashboard`} component={Dashboard} />
-        <Route path={`${path}/usuarios`} component={Users} />
-        <Route path={`${path}/entrevistas`} component={Interviews} />
+        {show()}
+        {/* {show()} */}
+        {/* {()=>show} */}
+        {/* {()=>show()} */}
+        {/* <Route path={`${path}/dashboard`} component={Dashboard} /> */}
+        {/* <Route path={`${path}/dashboard`} component={Dashboard} /> */}
+        {/* <Route path={`${path}/usuarios`} component={Users} /> */}
+        {/* <Route path={`${path}/entrevistas`} component={Interviews} /> */}
       </Switch>
     </DashboardContainer>
   )
