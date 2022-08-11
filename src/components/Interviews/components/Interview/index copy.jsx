eea0a363d884396e9f3b3cd0d650bc72e86d01e8
Anthony Romero <iaromerod@unac.edu.co>
2022-08-10 21:13:10 -0500

    add changes

--- src/components/Interviews/components/Interview/index copy.jsx
+++ src/components/Interviews/components/Interview/index copy.jsx
@@ -1,4 +1,3 @@
-// import { Table, TableBody, TableHead } from "@mui/material";
 import {
   useRouteMatch,
   Link,
@@ -12,115 +11,50 @@ import { NewInterview } from "../NewInterview";
 
 import { Action, Area, Card, Content, Crear, Img } from "./Interview.styles";
 
-// datos_quemados "
-// const data = [
-//   { id: 1, personaje: "Naruto", anime: "Naruto", genero: "Hombre" },
-//   { id: 2, personaje: "Goku", anime: "Dragon Ball", genero: "Hombre" },
-//   { id: 3, personaje: "Keshin Himura", anime: "One Piece", genero: "Hombre" },
-//   { id: 4, personaje: "Seto Kaibe", anime: "¡Yu-Gi-Oh!", genero: "Hombre" },
-//   { id: 5, personaje: "Natsu", anime: "Fairy Tail", genero: "Hombre" },
-// ];"
-
-export const Interviews = () => {
-  // The `path` lets us build <Route> paths that are
-  // relative to the parent route, while the `url` lets
-  // us build relative links.
+export const Interviews = ({
+  _id,
+  idInterview,
+  topic,
+  topicDescription,
+  actionsDescription,
+  referralDepartment,
+  userCreate,
+  userUpdate,
+  __typename
+}) => {
   let { path, url } = useRouteMatch();
   const { sidebar } = useUser();
   let { interviewId } = useParams();
-  // datos_quemados"
-  //  let state = {
-  //   data: data,
-  // };"
 
   return (
     <Switch>
       <Route exact path={path}>
         <Main title={"Entrevistas"} sidebar={sidebar}>
-          {/* <h2>Entrevistas</h2> */}
           <Crear>
             <Link to={`${url}/crear-entrevista`}>
               Crear una entrevista nueva
             </Link>
           </Crear>
-          {/* <h3>A continuación una lista de todas las entrevistas.</h3> */}
-
           <Card>
             <Area>
               <Img src="../../src/assets/img/FotoX.png" alt="beneficiario" />
               <Content>
                 <h3>
-                  Pedro
+                  {topic}
                 </h3>
                 <h4>35 años</h4>
                 <h4>3214567890</h4>
-                <h4>Ing. Sistemas</h4>
-                <h5>Problemas academicos</h5>
+                <h4>{referralDepartment}</h4>
+                <h5>{actionsDescription}</h5>
                 <p>
-                  No se como ser mas dedicado al estudio....
+                 {topicDescription}
                 </p>
               </Content>
             </Area>
             <div>
-              <Action>Ver mas</Action>
+              <Action to={`/entrevista/${idInterview}`}>Ver mas</Action>
             </div>
           </Card>
-
-          {/* <Table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>E-mail</th>
-                <th>Numero Telefonico</th>
-                <th>Genero</th>
-                <th>actions</th>
-              </tr>
-            </thead>
-            <Body>
-              <tr>
-                <td>1</td>
-                <td>Naruto</td>
-                <td>Naruto</td>
-                <td>3214567890</td>
-                <td>Hombre</td>
-                <td>
-                  <select>
-                    <option>. . .</option>
-                    <option>Edit</option>
-                    <option>Delete</option>
-                  </select>
-                </td>
-              </tr>
-            </Body>
-            {/* <tbody class="datos_quemados">
-              {this.state.data.map((elemento) => (
-                <tr>
-                  <td>{elemento.id}</td>
-                  <td>{elemento.personaje}</td>
-                  <td>{elemento.anime}</td>
-                  <td>{elemento.genero}</td>
-                  <td>
-                    <select>
-                      <option>Edit</option>
-                      <option>Delete</option>
-                    </select>
-                  </td>
-                </tr>
-              ))}
-            </tbody> */}
-          {/* </Table> */}
-          {/* <ul>
-            <li>
-              <Link to={`${url}/1`}>Bla bla bla</Link>
-            </li>
-            <li>
-              <Link to={`${url}/2`}>Bla bla bla</Link>
-            </li>
-            <li>
-              <Link to={`${url}/3`}>Bla bla bla</Link>
-            </li>
-          </ul> */}
         </Main>
       </Route>
 
